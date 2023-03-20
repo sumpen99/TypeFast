7d711c4e17e20e0dca37eac1a3d8516600fb7aa0
sumpen99 <sumpen841001@hotmail.com>
2023-03-20 16:01:20 +0100

    Initial Commit

---
+++ TypeFast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TypeFast
+//
+//  Created by fredrik sundström on 2023-03-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
