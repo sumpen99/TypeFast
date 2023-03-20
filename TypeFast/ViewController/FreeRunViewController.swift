7e84997cbce0261d2c08868b9301fd9b6c6abfde
sumpen99 <sumpen841001@hotmail.com>
2023-03-20 23:01:42 +0100

    LaunchScreen

---
+++ TypeFast/ViewController/FreeRunViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FreeRunViewController.swift
+//  TypeFast
+//
+//  Created by fredrik sundström on 2023-03-20.
+//
+
+import UIKit
+
+class FreeRunViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
